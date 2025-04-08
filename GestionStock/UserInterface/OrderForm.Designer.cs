667e66e446d0600b85cc352b64357c68c639aacb
P-S.Cypre <pscypre@gmail.com>
2025-04-08 00:04:14 -0400

    Complétion OrderForm

--- GestionStock/UserInterface/OrderForm.Designer.cs
+++ GestionStock/UserInterface/OrderForm.Designer.cs
@@ -39,6 +39,24 @@ partial class OrderForm
         this.columnHeader3 = new ColumnHeader();
         this.columnHeader4 = new ColumnHeader();
         this.btnProcessNextOrder = new Button();
+        this.groupBox1 = new GroupBox();
+        this.lsvNewOrderItems = new ListView();
+        this.columnHeader5 = new ColumnHeader();
+        this.columnHeader6 = new ColumnHeader();
+        this.columnHeader7 = new ColumnHeader();
+        this.columnHeader8 = new ColumnHeader();
+        this.label4 = new Label();
+        this.btnAddItemToOrder = new Button();
+        this.numQuantityToAdd = new NumericUpDown();
+        this.label3 = new Label();
+        this.cmbAvailableProducts = new ComboBox();
+        this.label2 = new Label();
+        this.txtCustomerName = new TextBox();
+        this.label1 = new Label();
+        this.btnPlaceOrder = new Button();
+        this.btnCancelNewOrder = new Button();
+        this.groupBox1.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)this.numQuantityToAdd).BeginInit();
         this.SuspendLayout();
         // 
         // lblQueueInfo
@@ -126,17 +144,164 @@ partial class OrderForm
         this.btnProcessNextOrder.Enabled = false;
         this.btnProcessNextOrder.Location = new Point(18, 396);
         this.btnProcessNextOrder.Name = "btnProcessNextOrder";
-        this.btnProcessNextOrder.Size = new Size(770, 42);
+        this.btnProcessNextOrder.Size = new Size(759, 83);
         this.btnProcessNextOrder.TabIndex = 10;
         this.btnProcessNextOrder.Text = "Traiter la Prochaine Commande";
         this.btnProcessNextOrder.UseVisualStyleBackColor = true;
         this.btnProcessNextOrder.Click += this.btnProcessNextOrder_Click;
         // 
+        // groupBox1
+        // 
+        this.groupBox1.Controls.Add(this.lsvNewOrderItems);
+        this.groupBox1.Controls.Add(this.label4);
+        this.groupBox1.Controls.Add(this.btnAddItemToOrder);
+        this.groupBox1.Controls.Add(this.numQuantityToAdd);
+        this.groupBox1.Controls.Add(this.label3);
+        this.groupBox1.Controls.Add(this.cmbAvailableProducts);
+        this.groupBox1.Controls.Add(this.label2);
+        this.groupBox1.Controls.Add(this.txtCustomerName);
+        this.groupBox1.Controls.Add(this.label1);
+        this.groupBox1.Location = new Point(794, 12);
+        this.groupBox1.Name = "groupBox1";
+        this.groupBox1.Size = new Size(344, 391);
+        this.groupBox1.TabIndex = 11;
+        this.groupBox1.TabStop = false;
+        this.groupBox1.Text = "Créer une Nouvelle Commande";
+        // 
+        // lsvNewOrderItems
+        // 
+        this.lsvNewOrderItems.Columns.AddRange(new ColumnHeader[] { this.columnHeader5, this.columnHeader6, this.columnHeader7, this.columnHeader8 });
+        this.lsvNewOrderItems.FullRowSelect = true;
+        this.lsvNewOrderItems.GridLines = true;
+        this.lsvNewOrderItems.Location = new Point(6, 220);
+        this.lsvNewOrderItems.Name = "lsvNewOrderItems";
+        this.lsvNewOrderItems.Size = new Size(324, 161);
+        this.lsvNewOrderItems.TabIndex = 8;
+        this.lsvNewOrderItems.UseCompatibleStateImageBehavior = false;
+        this.lsvNewOrderItems.View = View.Details;
+        // 
+        // columnHeader5
+        // 
+        this.columnHeader5.Text = "Produit";
+        this.columnHeader5.Width = 100;
+        // 
+        // columnHeader6
+        // 
+        this.columnHeader6.Text = "Qté";
+        this.columnHeader6.Width = 80;
+        // 
+        // columnHeader7
+        // 
+        this.columnHeader7.Text = "Prix U.";
+        this.columnHeader7.Width = 80;
+        // 
+        // columnHeader8
+        // 
+        this.columnHeader8.Text = "ID Produit";
+        // 
+        // label4
+        // 
+        this.label4.AutoSize = true;
+        this.label4.Location = new Point(15, 202);
+        this.label4.Name = "label4";
+        this.label4.Size = new Size(149, 15);
+        this.label4.TabIndex = 7;
+        this.label4.Text = "Articles de la Commande : ";
+        // 
+        // btnAddItemToOrder
+        // 
+        this.btnAddItemToOrder.Enabled = false;
+        this.btnAddItemToOrder.Location = new Point(15, 167);
+        this.btnAddItemToOrder.Name = "btnAddItemToOrder";
+        this.btnAddItemToOrder.Size = new Size(258, 23);
+        this.btnAddItemToOrder.TabIndex = 6;
+        this.btnAddItemToOrder.Text = "Ajouter au Panier";
+        this.btnAddItemToOrder.UseVisualStyleBackColor = true;
+        this.btnAddItemToOrder.Click += this.btnAddItemToOrder_Click;
+        // 
+        // numQuantityToAdd
+        // 
+        this.numQuantityToAdd.Location = new Point(74, 124);
+        this.numQuantityToAdd.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+        this.numQuantityToAdd.Name = "numQuantityToAdd";
+        this.numQuantityToAdd.Size = new Size(199, 23);
+        this.numQuantityToAdd.TabIndex = 5;
+        this.numQuantityToAdd.Value = new decimal(new int[] { 1, 0, 0, 0 });
+        // 
+        // label3
+        // 
+        this.label3.AutoSize = true;
+        this.label3.Location = new Point(6, 126);
+        this.label3.Name = "label3";
+        this.label3.Size = new Size(62, 15);
+        this.label3.TabIndex = 4;
+        this.label3.Text = "Quantité : ";
+        // 
+        // cmbAvailableProducts
+        // 
+        this.cmbAvailableProducts.DropDownStyle = ComboBoxStyle.DropDownList;
+        this.cmbAvailableProducts.FormattingEnabled = true;
+        this.cmbAvailableProducts.Location = new Point(6, 85);
+        this.cmbAvailableProducts.Name = "cmbAvailableProducts";
+        this.cmbAvailableProducts.Size = new Size(267, 23);
+        this.cmbAvailableProducts.TabIndex = 3;
+        this.cmbAvailableProducts.SelectedIndexChanged += this.cmbAvailableProducts_SelectedIndexChanged;
+        // 
+        // label2
+        // 
+        this.label2.AutoSize = true;
+        this.label2.Location = new Point(6, 67);
+        this.label2.Name = "label2";
+        this.label2.Size = new Size(115, 15);
+        this.label2.TabIndex = 2;
+        this.label2.Text = "Produits Disponibles";
+        // 
+        // txtCustomerName
+        // 
+        this.txtCustomerName.Location = new Point(80, 28);
+        this.txtCustomerName.Name = "txtCustomerName";
+        this.txtCustomerName.Size = new Size(193, 23);
+        this.txtCustomerName.TabIndex = 1;
+        this.txtCustomerName.TextChanged += this.txtCustomerName_TextChanged;
+        // 
+        // label1
+        // 
+        this.label1.AutoSize = true;
+        this.label1.Location = new Point(6, 31);
+        this.label1.Name = "label1";
+        this.label1.Size = new Size(68, 15);
+        this.label1.TabIndex = 0;
+        this.label1.Text = "Nom Client";
+        // 
+        // btnPlaceOrder
+        // 
+        this.btnPlaceOrder.Enabled = false;
+        this.btnPlaceOrder.Location = new Point(800, 399);
+        this.btnPlaceOrder.Name = "btnPlaceOrder";
+        this.btnPlaceOrder.Size = new Size(169, 80);
+        this.btnPlaceOrder.TabIndex = 12;
+        this.btnPlaceOrder.Text = "Passer la Commande";
+        this.btnPlaceOrder.UseVisualStyleBackColor = true;
+        this.btnPlaceOrder.Click += this.btnPlaceOrder_Click;
+        // 
+        // btnCancelNewOrder
+        // 
+        this.btnCancelNewOrder.Location = new Point(975, 399);
+        this.btnCancelNewOrder.Name = "btnCancelNewOrder";
+        this.btnCancelNewOrder.Size = new Size(151, 80);
+        this.btnCancelNewOrder.TabIndex = 13;
+        this.btnCancelNewOrder.Text = "Annuler";
+        this.btnCancelNewOrder.UseVisualStyleBackColor = true;
+        this.btnCancelNewOrder.Click += this.btnCancelNewOrder_Click;
+        // 
         // OrderForm
         // 
         this.AutoScaleDimensions = new SizeF(7F, 15F);
         this.AutoScaleMode = AutoScaleMode.Font;
-        this.ClientSize = new Size(800, 450);
+        this.ClientSize = new Size(1147, 491);
+        this.Controls.Add(this.btnCancelNewOrder);
+        this.Controls.Add(this.btnPlaceOrder);
+        this.Controls.Add(this.groupBox1);
         this.Controls.Add(this.btnProcessNextOrder);
         this.Controls.Add(this.lsvOrderItems);
         this.Controls.Add(this.label10);
@@ -147,6 +312,9 @@ partial class OrderForm
         this.Name = "OrderForm";
         this.Text = "OrderForm";
         this.Load += this.OrderForm_Load;
+        this.groupBox1.ResumeLayout(false);
+        this.groupBox1.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize)this.numQuantityToAdd).EndInit();
         this.ResumeLayout(false);
         this.PerformLayout();
     }
@@ -163,4 +331,20 @@ partial class OrderForm
     private ColumnHeader columnHeader3;
     private ColumnHeader columnHeader4;
     private Button btnProcessNextOrder;
+    private GroupBox groupBox1;
+    private Label label1;
+    private ListView lsvNewOrderItems;
+    private Label label4;
+    private Button btnAddItemToOrder;
+    private NumericUpDown numQuantityToAdd;
+    private Label label3;
+    private ComboBox cmbAvailableProducts;
+    private Label label2;
+    private TextBox txtCustomerName;
+    private ColumnHeader columnHeader5;
+    private ColumnHeader columnHeader6;
+    private ColumnHeader columnHeader7;
+    private ColumnHeader columnHeader8;
+    private Button btnPlaceOrder;
+    private Button btnCancelNewOrder;
 }
--- GestionStock/UserInterface/OrderForm.cs
+++ GestionStock/UserInterface/OrderForm.cs
@@ -14,6 +14,9 @@ namespace GestionStock.UserInterface;
 public partial class OrderForm : Form
 {
     private DataManager _dataManager;
+    // Pour stocker les articles de la commande en cours de création
+    private List<OrderItem> _currentOrderItems = new List<OrderItem>();
+    private Product _selectedAvailableProduct = null; // Produit sélectionné dans le ComboBox
 
     public OrderForm(DataManager dataManager)
     {
@@ -23,7 +26,9 @@ public partial class OrderForm : Form
 
     private void OrderForm_Load(object sender, EventArgs e)
     {
-        RefreshOrderView();
+        RefreshOrderView(); // Met à jour la vue de la file d'attente existante
+        PopulateAvailableProducts(); // Charge les produits dans le ComboBox de création
+        ClearNewOrderForm(); // Assure un état initial propre pour la création
     }
 
     // Met à jour tout l'affichage du formulaire
@@ -127,4 +132,221 @@ public partial class OrderForm : Form
             RefreshOrderView();
         }
     }
+
+    // --- FIN Logique de la file d'attente existante ---
+    // Charge les produits dans le ComboBox
+    private void PopulateAvailableProducts()
+    {
+        try
+        {
+            List<Product> products = _dataManager.GetAllProducts();
+            // Filtrer les produits en stock (optionnel mais recommandé)
+            List<Product> availableProducts = new List<Product>();
+            foreach (Product p in products)
+            {
+                if (p.Quantity > 0)
+                {
+                    availableProducts.Add(p);
+                }
+            }
+
+            cmbAvailableProducts.DataSource = null;
+            cmbAvailableProducts.DataSource = availableProducts; // N'afficher que ceux en stock
+                                                                 // Utiliser ToString() du produit pour l'affichage (il faut qu'il soit informatif)
+                                                                 // Ou configurer DisplayMember si vous avez une propriété spécifique
+            cmbAvailableProducts.DisplayMember = "Name"; // Ou une propriété plus descriptive
+            cmbAvailableProducts.ValueMember = "Id";
+            cmbAvailableProducts.SelectedIndex = -1; // Pas de sélection initiale
+            _selectedAvailableProduct = null;
+            btnAddItemToOrder.Enabled = false;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Erreur lors du chargement des produits disponibles: {ex.Message}", "Erreur Produits", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private void cmbAvailableProducts_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (cmbAvailableProducts.SelectedItem != null)
+        {
+            _selectedAvailableProduct = (Product)cmbAvailableProducts.SelectedItem;
+            // Ajuster le max du NumericUpDown à la quantité dispo
+            numQuantityToAdd.Maximum = _selectedAvailableProduct.Quantity;
+            numQuantityToAdd.Value = 1; // Réinitialiser à 1
+            btnAddItemToOrder.Enabled = true; // Activer le bouton d'ajout
+        }
+        else
+        {
+            _selectedAvailableProduct = null;
+            btnAddItemToOrder.Enabled = false;
+            numQuantityToAdd.Maximum = 1; // Valeur sûre si rien n'est sélectionné
+            numQuantityToAdd.Value = 1;
+        }
+        UpdatePlaceOrderButtonState();
+    }
+
+    private void btnAddItemToOrder_Click(object sender, EventArgs e)
+    {
+        if (_selectedAvailableProduct == null)
+        {
+            MessageBox.Show("Veuillez sélectionner un produit.", "Aucun Produit", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+        }
+
+        int quantity = (int)numQuantityToAdd.Value;
+        if (quantity <= 0)
+        {
+            MessageBox.Show("La quantité doit être supérieure à zéro.", "Quantité Invalide", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+        }
+        // Vérification supplémentaire (même si le max du NumericUpDown devrait le gérer)
+        if (quantity > _selectedAvailableProduct.Quantity)
+        {
+            MessageBox.Show($"Quantité insuffisante en stock pour '{_selectedAvailableProduct.Name}'. Disponible : {_selectedAvailableProduct.Quantity}", "Stock Insuffisant", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            numQuantityToAdd.Value = _selectedAvailableProduct.Quantity; // Mettre au max dispo
+            return;
+        }
+
+        // Créer l'objet OrderItem (important: utiliser le prix actuel du produit)
+        OrderItem newItem = new OrderItem(_selectedAvailableProduct.Id, quantity, _selectedAvailableProduct.Price);
+
+        // Ajouter à notre liste temporaire
+        // Vérifier si le produit est déjà dans le panier pour juste augmenter la quantité (optionnel)
+        bool found = false;
+        for (int i = 0; i < _currentOrderItems.Count; i++)
+        {
+            if (_currentOrderItems[i].ProductId == newItem.ProductId)
+            {
+                // Augmenter la quantité existante
+                // Vérifier si l'ajout dépasse le stock total !
+                int newTotalQuantity = _currentOrderItems[i].Quantity + newItem.Quantity;
+                if (newTotalQuantity > _selectedAvailableProduct.Quantity)
+                {
+                    MessageBox.Show($"Quantité totale ({newTotalQuantity}) dépasse le stock disponible ({_selectedAvailableProduct.Quantity}) pour '{_selectedAvailableProduct.Name}'.", "Stock Insuffisant", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return; // Ne pas ajouter
+                }
+                _currentOrderItems[i].Quantity = newTotalQuantity;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            _currentOrderItems.Add(newItem);
+        }
+
+
+        // Mettre à jour la ListView du panier
+        RefreshNewOrderItemsList();
+
+        // Réinitialiser pour le prochain ajout
+        cmbAvailableProducts.SelectedIndex = -1; // Désélectionner
+        numQuantityToAdd.Value = 1;
+        _selectedAvailableProduct = null;
+        btnAddItemToOrder.Enabled = false;
+
+        UpdatePlaceOrderButtonState(); // Mettre à jour l'état du bouton "Passer la commande"
+    }
+
+    // Met à jour la ListView lsvNewOrderItems
+    private void RefreshNewOrderItemsList()
+    {
+        lsvNewOrderItems.Items.Clear();
+        foreach (OrderItem item in _currentOrderItems)
+        {
+            Product product = _dataManager.GetProductById(item.ProductId);
+            string productName = (product != null) ? product.Name : "Produit Inconnu";
+
+            ListViewItem viewItem = new ListViewItem(productName);
+            viewItem.SubItems.Add(item.Quantity.ToString());
+            viewItem.SubItems.Add(item.PriceAtOrder.ToString("C"));
+            viewItem.SubItems.Add(item.ProductId.ToString()); // ID caché
+            viewItem.Tag = item; // Stocker l'objet OrderItem si besoin (ex: pour suppression)
+            lsvNewOrderItems.Items.Add(viewItem);
+        }
+    }
+
+    // Activer/Désactiver le bouton "Passer la Commande"
+    private void UpdatePlaceOrderButtonState()
+    {
+        bool customerNameOk = !string.IsNullOrWhiteSpace(txtCustomerName.Text);
+        bool itemsOk = _currentOrderItems.Count > 0;
+        btnPlaceOrder.Enabled = customerNameOk && itemsOk;
+    }
+
+    private void txtCustomerName_TextChanged(object sender, EventArgs e)
+    {
+        UpdatePlaceOrderButtonState();
+    }
+
+
+    private void btnPlaceOrder_Click(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(txtCustomerName.Text))
+        {
+            MessageBox.Show("Veuillez entrer un nom de client.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            txtCustomerName.Focus();
+            return;
+        }
+        if (_currentOrderItems.Count == 0)
+        {
+            MessageBox.Show("Veuillez ajouter au moins un produit à la commande.", "Panier Vide", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+        }
+
+        try
+        {
+            // Créer la nouvelle commande
+            Order newOrder = new Order(0, txtCustomerName.Text.Trim()); // ID sera généré
+                                                                        // Important : Cloner la liste d'items pour éviter les références partagées
+            newOrder.Items = new List<OrderItem>(_currentOrderItems);
+
+            // Ajouter la commande à la file d'attente via DataManager
+            _dataManager.AddOrder(newOrder);
+
+            MessageBox.Show($"Commande #{newOrder.Id} pour '{newOrder.CustomerName}' ajoutée à la file d'attente.", "Commande Passée", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Réinitialiser le formulaire de nouvelle commande
+            ClearNewOrderForm();
+
+            // Mettre à jour l'affichage de la file d'attente principale
+            RefreshOrderView();
+
+            // Recharger les produits disponibles (le stock a peut-être changé si AddOrder le modifiait,
+            // mais idéalement, le stock ne change qu'au traitement 'ProcessNextOrder')
+            // Si le stock est réduit à la commande, il faut recharger :
+            // PopulateAvailableProducts();
+            // Sinon, ce n'est pas nécessaire ici.
+        }
+        catch (InvalidOperationException iopEx) // Ex: Produit n'existe plus entre temps
+        {
+            MessageBox.Show(iopEx.Message, "Erreur Commande", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Erreur lors de la création de la commande : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private void btnCancelNewOrder_Click(object sender, EventArgs e)
+    {
+        ClearNewOrderForm();
+    }
+
+    // Réinitialise la section de création de commande
+    private void ClearNewOrderForm()
+    {
+        txtCustomerName.Clear();
+        cmbAvailableProducts.SelectedIndex = -1;
+        numQuantityToAdd.Value = 1;
+        _currentOrderItems.Clear();
+        lsvNewOrderItems.Items.Clear();
+        _selectedAvailableProduct = null;
+        btnAddItemToOrder.Enabled = false;
+        btnPlaceOrder.Enabled = false; // Désactiver le bouton Passer Commande
+    }
+
+
+
 }
